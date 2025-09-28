25a6d8c6f810ad43953393732f7cc52b55cae45a
RybchynskyiDaniil <rybchynskyidaniil@gmail.com>
2025-09-28 20:45:19 +0000

    Deploying to gh-pages from @ RybchynskyiDaniil/goit-js-hw-09@b59fbab58fbbb93be73ae4f437a3524a0d1dd522 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const a=document.querySelector(".feedback-form"),t={email:"",message:""};a.addEventListener("input",e=>{t[e.target.name]=e.target.value.trim(),localStorage.setItem("feedback-form-state",JSON.stringify(t))});
+//# sourceMappingURL=2-form.js.map
