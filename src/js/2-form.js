59bc700fa0c766760f5670f21462725f93dbc88d
Daniil Rybchynskyi <rybchynskyidaniil@gmail.com>
2025-10-01 20:13:15 +0100

    completed

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,5 +5,28 @@ const formData = {
 }
 elForm.addEventListener("input", (ev) => {
     formData[ev.target.name] = ev.target.value.trim();
-    localStorage.setItem("feedback-form-state" , JSON.stringify(formData))
-} )
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+})
+const saveData = localStorage.getItem("feedback-form-state")
+if (saveData) {
+    const parseEl = JSON.parse(saveData)
+    formData.email = parseEl.email
+    formData.message = parseEl.message
+
+    elForm.elements.email.value = parseEl.email
+    elForm.elements.message.value = parseEl.message
+}
+
+elForm.addEventListener("submit", (e) => {
+    e.preventDefault() 
+
+    if (formData.email === "" || formData.message === "") {
+        alert("Fill please all fields")
+        return
+    }
+    console.log(formData)
+    formData.email = ""
+    formData.message = ""
+    localStorage.removeItem("feedback-form-state")
+    elForm.reset()
+})
